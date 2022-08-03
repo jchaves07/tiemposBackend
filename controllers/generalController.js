0580b78ed18ba6e4afc0b5ba9a1ba2d22314f7c7
Jorge Chaves Ulate <jorge.chaves@me.com>
2022-08-02 18:24:23 -0600

    .

--- controllers/generalController.js
+++ controllers/generalController.js
@@ -81,3 +81,37 @@ exports.sendMail = async (req, res) => {
     });
     res.sendStatus(200);
 }
+exports.sendMailGeneral = async (req, res) => {
+    const {msg, subject} = req.body;
+
+    var request = require("request");
+    var options = {
+        method: 'POST',
+        url: 'https://api.sendgrid.com/v3/mail/send',
+        headers:
+        {
+            'Postman-Token': 'c6e2e63a-6e5f-43ac-821d-4483a6822717',
+            'cache-control': 'no-cache',
+            Authorization: 'Bearer ',
+            'Content-Type': 'application/json'
+        },
+        body:
+        {
+            personalizations:
+                [{
+                    to: [{ email: "nilton.obregon@agenciadatsun.com" }, { email: "angelica.rodriguez@agenciadatsun.com" }, { email: "billy.rodriguez@agenciadatsun.com" }, { email: "jorge.rojas@grupodanissa.com" }],
+                  cc: [{ email: "jorgek8001@gmail.com" }],
+                    subject: subject
+                }],
+            from: { email: 'agentevirtual@nissancr.com' },
+            content: [{ type: 'text/html', value: msg }]
+        },
+        json: true
+    };
+
+    request(options, function (error, response, msg) {
+        if (error) throw new Error(error);
+
+    });
+    res.sendStatus(200);
+}
--- routes/general.js
+++ routes/general.js
@@ -9,5 +9,6 @@ router.post('/sendmail', generalController.sendMail);
 router.post('/ReporteSemanal', validateSessionReq, auth, generalController.ReporteSemanal);
 router.get('/getSorteosDropdown',validateSessionReq, auth, generalController.getSorteosDropdown);
 router.get('/GetWeeks', validateSessionReq, auth, generalController.GetWeeks);
+router.post('/sendMailGeneral', generalController.sendMailGeneral);
 
 module.exports = router;
